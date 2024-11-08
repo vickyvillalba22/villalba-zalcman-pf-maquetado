36f0e0cd7c450f660feb4c97639de82505283691
CamilaZalcman <camilazalcman@gmail.com>
2024-11-08 12:21:09 -0300

    Cambios cami

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+/*let carrusel=[
+    {
+        imagenFondo: "assets/imgs/bolsa-plantas.jpg",
+        subtitulo: a,
+        titulo:    a ,
+        descripcion: a,
+        textoBoton: a,
+    },
+    {
+        imagenFondo: "assets/imgs/bolsa-plantas.jpg",
+        subtitulo:  a,
+        titulo:    a ,
+        descripcion: a,
+        textoBoton: a,
+    },
+    {
+        imagenFondo: "assets/imgs/bolsa-plantas.jpg",
+        subtitulo:  a,
+        titulo:    a ,
+        descripcion: a,
+        textoBoton: a,
+    }  
+]
+
+let currentFoto=0;
+
+let banner=document.getElementById("slider");
+updateBack()
+
+function updateBack(){
+    banner.style.backgroundImage="carrusel[i].imagenFondo"
+}
+
+
+
+
+for(let i=0; i<3; i++){
+
+    
+}*/
