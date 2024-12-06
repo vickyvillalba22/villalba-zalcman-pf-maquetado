e27abb32e1eb8283c6953bcbe2a6e3fa3d8ad95b
vickyvillalba <villalbavictoria120@gmail.com>
2024-12-05 23:48:09 -0300

    jueves1

---
+++ assets/js/home.js
@@ -0,0 +1,135 @@
+import { generarBlog, generar_customers, logica_customers } from "./script.js";
+
+const carrusel=[
+    {
+        imagenFondo: "assets/imgs/bolsa-plantas.jpg",
+        subtitulo: "deisgn & branding",
+        titulo:    "We are Floria Agency",
+        descripcion: "How you write your advertising copy will be based on where you will place your ad. If it’s a billboard ad, you’ll need a super catchy headline.",
+        textoBoton: "See Our Works"
+    },
+    {
+        imagenFondo: "assets/imgs/relojes-blancos.jpg",
+        subtitulo: "featured work",
+        titulo:    "AppStarter" ,
+        descripcion: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
+        textoBoton: "See Case Study"
+    },
+    {
+        imagenFondo: "assets/imgs/almohada-pajaro-flor.jpg",
+        subtitulo: "featured work",
+        titulo: "Delicious Café" ,
+        descripcion: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
+        textoBoton: "See Case Study"
+    }  
+];
+
+    //esto está afuera de una funcion porque este modulo solo va a funcionar en la home
+
+    let currentFoto=0;
+
+    let banner = document.getElementById("sliderA");
+    //console.log(banner);
+    banner.classList.add("vh90", "centerX", "centerY", "caract-Banner", "vh50q");
+    let botonIzq = document.getElementById("izquierda");
+    let botonDer = document.getElementById("derecha");
+
+   //div que agrupa todo
+    let divBanner = document.createElement("div");
+    divBanner.classList.add("textCenter", "colorBlanco", "columna", "centerY", "h20", "spacea", "w50", "w100m", "w70q");
+    banner.appendChild(divBanner);
+
+    //subtitulo
+    let chico = document.createElement("h3");
+    chico.classList.add("chicos");
+    divBanner.appendChild(chico);
+
+    //titulo principal
+    let titPrincipal = document.createElement("h1");
+    titPrincipal.classList.add("titPrincipal" , "fontSize3m", "lineaBlanca", "posicionRel", "df", "centerX", "hIconos", "centerY", "achicarPrincipal");
+    divBanner.appendChild(titPrincipal);
+
+    //descripcion
+    let descripcion = document.createElement("p");
+    descripcion.classList.add("parrafos");
+    divBanner.appendChild(descripcion);
+
+    //boton !! capaz hay que ponerle hacia donde va
+    let botonBanner = document.createElement("button");
+    botonBanner.classList.add("paddingBotones", "fondoBlanco", "fontBotones", "sinBorde", "fitContent");
+    divBanner.appendChild(botonBanner);
+
+
+function updateBack(){
+    
+    //img de fondo
+    banner.style.backgroundImage = `url(${carrusel[currentFoto].imagenFondo})`;
+
+    chico.innerHTML = carrusel[currentFoto].subtitulo;
+
+    titPrincipal.innerHTML = carrusel[currentFoto].titulo;
+
+    descripcion.innerHTML = carrusel[currentFoto].descripcion;
+
+    botonBanner.innerHTML = carrusel[currentFoto].textoBoton;
+
+}
+
+function siguiente(){
+
+    currentFoto++;
+
+    if (currentFoto>carrusel.length-1){
+        currentFoto=0;
+    }
+
+    updateBack();
+}
+
+function anterior(){
+
+    currentFoto--;
+
+    if (currentFoto<0){
+        currentFoto = carrusel.length-1;
+    }
+
+    updateBack();
+}
+
+botonIzq.addEventListener('click', () => {
+    pause();
+    anterior();
+} );
+botonDer.addEventListener('click', () => {
+    pause();
+    siguiente();
+} );
+
+updateBack()
+
+let interRapido;
+
+function rapido(){
+    interRapido = setInterval(siguiente, 5000);
+}
+
+function pause(){
+    clearInterval(interRapido);
+}
+
+rapido();
+
+
+/*CUSTOMERS ABOUT US*/
+
+let customersAboutUs = document.getElementById("custAboutUs");
+
+generar_customers(customersAboutUs);
+logica_customers();
+
+/*FROM OUR BLOG*/
+
+let contenedorBlog = document.getElementById("contenedorOurBlog");
+
+generarBlog(contenedorBlog);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,3 @@
-let carrusel=[
-    {
-        imagenFondo: "assets/imgs/bolsa-plantas.jpg",
-        subtitulo: "deisgn & branding",
-        titulo:    "We are Floria Agency",
-        descripcion: "How you write your advertising copy will be based on where you will place your ad. If it’s a billboard ad, you’ll need a super catchy headline.",
-        textoBoton: "See Our Works"
-    },
-    {
-        imagenFondo: "assets/imgs/relojes-blancos.jpg",
-        subtitulo: "featured work",
-        titulo:    "AppStarter" ,
-        descripcion: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        textoBoton: "See Case Study"
-    },
-    {
-        imagenFondo: "assets/imgs/almohada-pajaro-flor.jpg",
-        subtitulo: "featured work",
-        titulo: "Delicious Café" ,
-        descripcion: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        textoBoton: "See Case Study"
-    }  
-];
-
-let currentFoto=0;
-
-let banner = document.getElementById("sliderA");
-console.log(banner);
-banner.classList.add("vh90", "centerX", "centerY", "caract-Banner", "vh50q");
-let botonIzq = document.getElementById("izquierda");
-let botonDer = document.getElementById("derecha");
-
-   //div que agrupa todo
-    let divBanner = document.createElement("div");
-    divBanner.classList.add("textCenter", "colorBlanco", "columna", "centerY", "h20", "spacea", "w50", "w100m", "w70q");
-    banner.appendChild(divBanner);
-
-    //subtitulo
-    let chico = document.createElement("h3");
-    chico.classList.add("chicos");
-    divBanner.appendChild(chico);
-
-    //titulo principal
-    let titPrincipal = document.createElement("h1");
-    titPrincipal.classList.add("titPrincipal" , "fontSize3m", "lineaBlanca", "posicionRel", "df", "centerX", "hIconos", "centerY", "achicarPrincipal");
-    divBanner.appendChild(titPrincipal);
-
-    //descripcion
-    let descripcion = document.createElement("p");
-    descripcion.classList.add("parrafos");
-    divBanner.appendChild(descripcion);
-
-    //boton !! capaz hay que ponerle hacia donde va
-    let botonBanner = document.createElement("button");
-    botonBanner.classList.add("paddingBotones", "fondoBlanco", "fontBotones", "sinBorde", "fitContent");
-    divBanner.appendChild(botonBanner);
-
-updateBack()
-
-function updateBack(){
-    
-    //img de fondo
-    banner.style.backgroundImage = `url(${carrusel[currentFoto].imagenFondo})`;
-
-    chico.innerHTML = carrusel[currentFoto].subtitulo;
-
-    titPrincipal.innerHTML = carrusel[currentFoto].titulo;
-
-    descripcion.innerHTML = carrusel[currentFoto].descripcion;
-
-    botonBanner.innerHTML = carrusel[currentFoto].textoBoton;
-
-}
-
-function siguiente(){
-    currentFoto++;
-
-    if (currentFoto>carrusel.length-1){
-        currentFoto=0;
-    }
-
-    updateBack();
-}
-
-function anterior(){
-    currentFoto--;
-
-    if (currentFoto<0){
-        currentFoto = carrusel.length-1;
-    }
-
-    updateBack();
-}
-
-botonIzq.addEventListener('click', anterior);
-botonDer.addEventListener('click', siguiente);
-
-
-
 /*SECTION SOME OF OUR WORKS*/
 
 let trabajos=document.getElementById("ourWorks") //me traigo el contenedor
@@ -214,7 +115,7 @@ generarPortfolio()
 
 /*CUSTOMERS ABOUT US*/
 
-let customers = [
+const customers = [
     {
         imagen: "assets/imgs/chica-pelirroja.jpg",
         comentario: '“Magazine advertising is the most versatile, but this is solely dependent on the size of your ad and how many other ads compete with yours.”',
@@ -241,49 +142,59 @@ let customers = [
     }
 ];
 
-//creo y appendchileo todos los elementos html sin contenido pero con sus respectivas clases generales.
-let contenedor = document.createElement("div");
-contenedor.setAttribute("id", "contCustomers");
-contenedor.classList.add("df", "columna", "centerX", "centerY", "vh90","w100m", "vh50q" , "marginTop3m");
-
-let cuadrado = document.createElement("div"); 
-cuadrado.classList.add("opacidadBaja", "vh60", "w32", "posicionRel", "w80m", "vh30q", "w50q", "w90m", "vh80m");
-contenedor.appendChild(cuadrado);
-
-let customerTotal = document.createElement("div");
-customerTotal.classList.add("posicionAb", "df", "columna", "centerX", "centerY", "h50", "w40", "spacea","w100m", "w70q", "vh50q");
-contenedor.appendChild(customerTotal);
-
-let image = document.createElement("img");
-image.classList.add("redonda");
-
-let linea = document.createElement("div");
-linea.classList.add("lineaNaranja", "posicionRel", "w90q", "centerXq", "centerYq","dfQ", "centerX","centerY");
-
-let comment = document.createElement("p");
-comment.classList.add("parrafoTitulo", "fontSize1-2m", "w90m", "w90q");
-
-let datosCliente = document.createElement("div");
-datosCliente.setAttribute("class", "df", "marginBottom1m");
-let nombre = document.createElement("h4");
-nombre.classList.add("parrafos");
-let cargo = document.createElement("h4");
-cargo.classList.add("parrafos", "colorGris");
-datosCliente.append(nombre, cargo);
-
-customerTotal.append(image, comment, linea, datosCliente);
-
-let customersAboutUs = document.getElementById("custAboutUs");
-customersAboutUs.appendChild(contenedor);
-
-//botones
-let botonAnt = document.getElementById("anterior");
-let botonSig = document.getElementById("siguiente");
-
-//defino una variable que representa la posicion en el array.
+//defino las variables que se reutilizarán y actualizarán
 let currentClient = 0;
+let cuadrado;
+let image;
+let comment;
+let nombre;
+let cargo;
+let botonAnt;
+let botonSig;
 
-//llamo a la función de actualizar contenido para que ya cargue lo que se ve primero.
+//creo y appendchileo todos los elementos html sin contenido pero con sus respectivas clases generales. Las lineas que crean elementos pueden quedar fuera de funciones porque no traen nada de otros lados.
+
+export function generar_customers(caja){
+
+    console.log("hola")
+    
+    cuadrado = document.createElement("div"); 
+    cuadrado.classList.add("opacidadBaja", "vh60", "w32", "posicionAb", "w80m", "vh30q", "w50q", "w90m", "vh80m");
+    caja.appendChild(cuadrado);
+    
+    let customerTotal = document.createElement("div");
+    customerTotal.classList.add("posicionAb", "df", "columna", "centerX", "centerY", "vh", "w78", "spacea","w100m", "w70q", "vh50q");
+    
+    image = document.createElement("img");
+    image.classList.add("redonda");
+    
+    let linea = document.createElement("div");
+    linea.classList.add("lineaNaranja", "posicionRel", "w90q", "centerXq", "centerYq","dfQ", "centerX","centerY");
+    
+    comment = document.createElement("p");
+    comment.classList.add("parrafoTitulo", "w78", "fontSize1-2m", "w90m", "w90q");
+    
+    let datosCliente = document.createElement("div");
+    datosCliente.setAttribute("class", "df", "marginBottom1m");
+    nombre = document.createElement("h4");
+    nombre.classList.add("parrafos");
+    cargo = document.createElement("h4");
+    cargo.classList.add("parrafos", "colorGris");
+    datosCliente.append(nombre, cargo);
+    
+    customerTotal.append(image, comment, linea, datosCliente);
+    
+    caja.appendChild(customerTotal);
+    
+    //botones
+    botonAnt = document.getElementById("anterior");
+    botonSig = document.getElementById("siguiente");
+
+}
+
+
+export function logica_customers(){
+    //llamo a la función de actualizar contenido para que ya cargue lo que se ve primero.
 updateClient();
 
 //en esta funcion reemplazo el contenido según en qué posicion del recorrido del array esté currentClient. Y pongo clases específicas, pero se me acumulan las clases, asique lo tengo que ver.
@@ -321,11 +232,13 @@ function retrocede(){
 
 botonAnt.addEventListener('click', retrocede);
 botonSig.addEventListener('click', avanza);
+}
 
 
-/*SECTION FROM OUR BLOG - ME FALTAN COSAS!!*/
 
-/*let arrayBlog=[
+/*SECTION FROM OUR BLOG*/
+
+const arrayBlog=[
     {
         link: "marketing.html",
         imagenSrc: "assets/imgs/libros-lampara-rara.jpg",
@@ -369,13 +282,10 @@ botonSig.addEventListener('click', avanza);
     
 ]
 
-let contenedorBlog=document.getElementById("contenedorOurBlog");
-let contenedorBlog2 = document.getElementById("contenedorOurBlog2");
-console.log("hola"); //me traigo el contenedor
+/*let contenedorBlog2 = document.getElementById("contenedorOurBlog2");
+console.log(contenedorBlog2); //me traigo el contenedor*/
 
-function generarBlog(contenedor){
-
-    console.log("entra a generarBlog");
+export function generarBlog(contenedor){
     
     for(let i=0; i<arrayBlog.length; i++){
         let articulo=document.createElement("article"); //creo el articulo
@@ -446,10 +356,9 @@ function generarBlog(contenedor){
 
         articulo.appendChild(divInferior);
         contenedor.appendChild(articulo);
+        contenedor.classList.add("marginTop1")
         
     }
 
 }
 
-generarBlog(contenedorBlog);
-generarBlog(contenedorBlog2);*/
