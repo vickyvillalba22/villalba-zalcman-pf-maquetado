25249a22f053c4ba90e5d27be68dd0d7e1616e3d
vickyvillalba <villalbavictoria120@gmail.com>
2024-12-15 14:23:22 -0300

    js-floria

--- assets/js/blog-item.js
+++ assets/js/blog-item.js
@@ -1,4 +1,4 @@
-/*FROM OUR BLOG CON FILTROS*/
+/*YOU MIGHT ALSO LIKE - FROM OUR BLOG*/ 
 
 let contenedorBlog = document.getElementById("contenedorOurBlog");
 
@@ -13,7 +13,7 @@ const arrayBlog=[
         parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
         fecha:"Jan 25, 2019",
         fotoSrc: "assets/imgs/hombre-azul-1.jpg",
-        nombre: "John Davidson"
+        nombre: "John Davidson",
     },
 
     {
@@ -26,7 +26,7 @@ const arrayBlog=[
         parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
         fecha:"Jan 18, 2019",
         fotoSrc: "assets/imgs/mujer-rosa1.jpg",
-        nombre: "Christina Parker"
+        nombre: "Christina Parker",
     },
 
     {
@@ -39,99 +39,19 @@ const arrayBlog=[
         parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
         fecha:"Jan 10, 2019",
         fotoSrc: "assets/imgs/mujer1.jpg",
-        nombre: "Pamela Riley"
+        nombre: "Pamela Riley",
     } ,
 
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/chocolatada.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Tutorial",
-        h3Color: "colorVerde",
-        h2:"A great way to generate all the motivation you need to get fit",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 25, 2019",
-        fotoSrc: "assets/imgs/hombre-azul-1.jpg",
-        nombre: "John Davidson"
-    } ,
-
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/madre-e-hija.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Marketing",
-        h3Color: "colorRosa",
-        h2:"Kindness and motivation tips challenge groups to make a difference",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 18, 2019",
-        fotoSrc: "assets/imgs/mujer-rosa1.jpg",
-        nombre: "Christina Parker"
-    } ,
-
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/plantas-muebles.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Design",
-        h3Color: "colorVioleta",
-        h2:"When you are down and out how do you get up and go forward",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 25, 2019",
-        fotoSrc: "assets/imgs/hombre-azul-1.jpg",
-        nombre: "John Davidson"
-    } ,
-
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/caballs.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Productivity",
-        h3Color: "colorNaranja",
-        h2:"Hypnosis 12 steps to acquire mind power",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 10, 2019",
-        fotoSrc: "assets/imgs/mujer1.jpg",
-        nombre: "Pamela Riley"
-    } ,
-
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/chicos-saltando.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Tutorial",
-        h3Color: "colorVerde",
-        h2:"Living in the now. Use it enrich your life",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 18, 2019",
-        fotoSrc: "assets/imgs/mujer-rosa1.jpg",
-        nombre: "Christina Parker"
-    } ,
-
-    {
-        link: "blog-item.html",
-        imagenSrc: "assets/imgs/mar-montana.jpg",
-        imagenAlt: "cuadro que dice today is awesome, con lampara rara",
-        h3Texto: "Design",
-        h3Color: "colorVioleta",
-        h2:"Change your mind, change your luck",
-        parrafo: "A successful marketing plan relies heavily on the pulling-power of advertising copy. Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.",
-        fecha:"Jan 18, 2019",
-        fotoSrc: "assets/imgs/mujer-rosa1.jpg",
-        nombre: "Christina Parker"
-    }   
+    
     
 ]
 
-function generarBlog(inicio, cantidad, filtro=null){
-
-    contenedorBlog.innerHTML="";
+function generarBlog(){
     
-    for(let i=inicio; i<cantidad; i++){
-
-        if (filtro && arrayBlog[i].h3Texto !== filtro) continue;
+    for(let i=0; i<arrayBlog.length; i++){
 
         let articulo=document.createElement("article"); //creo el articulo
-        articulo.classList.add("w32", "vh70fijo", "df", "columna", "spaceb", "w45q", "w100m", "marginTopQ", "marginTop3m", "marginTop4") //le agrego las clases generales
+        articulo.classList.add("w32", "vh70fijo", "df", "columna", "spaceb", "w45q", "w100m", "marginTopQ", "marginTop3m") //le agrego las clases generales
 
         let enlace = document.createElement("a");
         enlace.classList.add("posicionRel")
@@ -201,71 +121,10 @@ function generarBlog(inicio, cantidad, filtro=null){
         contenedorBlog.classList.add("marginTop1")
         
     }
+
 }
 
-generarBlog(0, 6); 
-
-let cajaBotones = document.getElementById("contBotones");
-let botonNext = document.getElementById("buttonNext");
-let botonPrev = document.createElement("button");
-botonPrev.classList.add("fitContent", "fontBotones", "paddingBotones", "fondoBlanco", "df", "hForms", "centerY", "centerX");
-
-botonNext.addEventListener('click', () => {
-    console.log("Cargando más artículos...");
-    
-    // Limpiar el contenedor
-    contenedorBlog.innerHTML = "";
-
-    // Cargar más artículos
-    generarBlog(6, 9);
-
-    cajaBotones.innerHTML ="";
-
-    //armo el nuevo boton
-    botonPrev.innerHTML = "Previous";
-    cajaBotones.appendChild(botonPrev);
-    
-});
-
-botonPrev.addEventListener('click', ()=>{
-
-    // Limpiar el contenedor
-    contenedorBlog.innerHTML = "";
-
-    // Cargar más artículos
-    generarBlog(0, 6);
-
-    cajaBotones.innerHTML ="";
-
-    cajaBotones.appendChild(botonNext);
-
-});
-
-let botonAll = document.getElementById("all");
-let botonDesign = document.getElementById("design");
-let botonMarketing = document.getElementById("marketing");
-let botonPro = document.getElementById("productivity");
-let botonTutorial = document.getElementById("tutorial");
-
-botonAll.addEventListener('click', ()=>{
-    generarBlog(0, 9);
-})
-
-botonDesign.addEventListener('click', ()=>{
-    generarBlog(0, 9, "Design");
-})
-
-botonMarketing.addEventListener('click', ()=>{
-    generarBlog(0, 9, "Marketing");
-})
-
-botonPro.addEventListener('click', ()=>{
-    generarBlog(0, 9, "Productivity");
-})
-
-botonTutorial.addEventListener('click', ()=>{
-    generarBlog(0, 9, "Tutorial");
-})
+generarBlog(); 
 
 /*INSTAGRAM*/
 
@@ -335,5 +194,3 @@ function armar_instagram(){
 }
 
 armar_instagram();
-
-
